26aa7d29fb027f11d8aa7928e94a923cb026eabb
Chsvk <chsvk21@gmail.com>
2018-07-12 23:34:25 +0530

    working zombie-AR apks included

---
+++ Assets/Zombie_Walks.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class click : MonoBehaviour, IVirtualButtonEventHandler {
+
+	private GameObject VButtonObject;
+	private GameObject zombie;
+	// Use this for initialization
+	void Start () {
+		VButtonObject = GameObject.Find("VirtualButton");
+		zombie = GameObject.Find("zombie");
+		VButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+	}
+
+	public void OnButtonPressed (VirtualButtonAbstractBehaviour vb){
+		Debug.Log("DOWN");
+		zombie.GetComponent<Animation>().Play();
+	}
+
+	public void OnButtonReleased (VirtualButtonAbstractBehaviour vb){
+		zombie.GetComponent<Animation>().Stop();
+	}
+}
